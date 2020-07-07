36e8aea9fbad1362e9f5fa7cf87991f8e97cc658
Jitu <jittu.cse@gmail.com>
2020-07-08 03:28:05 +0530

    FIX

--- server.js
+++ server.js
@@ -21,72 +21,72 @@ app.get("/", (req, res) => {
 });
 app.use("/user", user);
 var http = require('http').createServer(app);
-// var io = require('socket.io')(http);
-// const chatUsers = require('./model/chatUsers');
-// const getUserFromToken = require('./auth');
-// InitiateMongoServer().then(
-//     ()=> {
-//
-//         io.on('connection', socket => {
-//             //Get the chatID of the user and join in a room of the same chatID
-//             console.log("new connnn");
-//             var userId = getUserFromToken(socket.handshake.query.token);
-//             socket.join(userId);
-//             try {
-//                 chatUsers.data[userId].online = true;
-//             } catch (e) {
-//                 SetChatUsers();
-//             }
-//
-//             //Leave the room if the user closes the socket
-//             socket.on('disconnect', () => {
-//                 console.log('disconnected');
-//                 try {
-//                     chatUsers.data[userId].online = false;
-//                     io.emit('chat-users', chatUsers.data);
-//                 } catch (e) {
-//
-//                 }
-//                 socket.leave(userId, ()=>{})
-//             });
-//
-//             socket.on('set_available', id => {
-//                 if (chatUsers.data.hasOwnProperty(id)) {
-//                     chatUsers.data[id].online = true;
-//                 }
-//             });
-//
-//             socket.on('available_users', () => {
-//                 io.emit('chat-users', chatUsers.data);
-//             });
-//
-//             //Leave the room if the user closes the socket
-//             socket.on('users', () => {
-//                 socket.leave(userId, ()=>{})
-//             });
-//
-//             //Send message to only a particular user
-//             socket.on('send_message', message => {
-//                 receiverChatID = message.receiverChatID;
-//                 senderChatID = message.senderChatID;
-//                 content = message.content;
-//
-//                 //Send message to only that particular room
-//                 socket.in(receiverChatID).emit('receive_message', {
-//                     'content': content,
-//                     'senderChatID': senderChatID,
-//                     'receiverChatID': receiverChatID,
-//                 })
-//             })
-//         });
-//         http.listen(PORT, (req, res) => {
-//             console.log(`Server Started at PORT ${PORT}`);
-//         });
-//     }
-// );
+var io = require('socket.io')(http);
+const chatUsers = require('./model/chatUsers');
+const getUserFromToken = require('./auth');
+InitiateMongoServer().then(
+    ()=> {
+
+        io.on('connection', socket => {
+            //Get the chatID of the user and join in a room of the same chatID
+            console.log("new connnn");
+            var userId = getUserFromToken(socket.handshake.query.token);
+            socket.join(userId);
+            try {
+                chatUsers.data[userId].online = true;
+            } catch (e) {
+                SetChatUsers();
+            }
+
+            //Leave the room if the user closes the socket
+            socket.on('disconnect', () => {
+                console.log('disconnected');
+                try {
+                    chatUsers.data[userId].online = false;
+                    io.emit('chat-users', chatUsers.data);
+                } catch (e) {
+
+                }
+                socket.leave(userId, ()=>{})
+            });
+
+            socket.on('set_available', id => {
+                if (chatUsers.data.hasOwnProperty(id)) {
+                    chatUsers.data[id].online = true;
+                }
+            });
+
+            socket.on('available_users', () => {
+                io.emit('chat-users', chatUsers.data);
+            });
+
+            //Leave the room if the user closes the socket
+            socket.on('users', () => {
+                socket.leave(userId, ()=>{})
+            });
+
+            //Send message to only a particular user
+            socket.on('send_message', message => {
+                receiverChatID = message.receiverChatID;
+                senderChatID = message.senderChatID;
+                content = message.content;
+
+                //Send message to only that particular room
+                socket.in(receiverChatID).emit('receive_message', {
+                    'content': content,
+                    'senderChatID': senderChatID,
+                    'receiverChatID': receiverChatID,
+                })
+            })
+        });
+        http.listen(PORT, (req, res) => {
+            console.log(`Server Started at PORT ${PORT}`);
+        });
+    }
+);
 
 
-// SetChatUsers();
+SetChatUsers();
 http.listen(PORT, (req, res) => {
             console.log(`Server Started at PORT ${PORT}`);
         });
